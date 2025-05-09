970b2ec4121a3c3178526f0e433031cd6cbacab5
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-05-09 06:21:44 +0530

    first commit

---
+++ Exercises/Day1 Exercise/swap_variables.py
@@ -0,0 +1,9 @@
+a = input("a : ")
+b = input("b : ")
+
+c = a
+a = b
+b = c
+
+print(f"a : {a}")
+print(f"b : {b}")
---
+++ Project/brand_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Brand name Generator.")
+city = input("Which city you grow up in?:\n")
+pet = input("What is the name of the pet?:\n")
+brand_name = city + pet
+print(f"Your Brand Name:\n{brand_name}")
